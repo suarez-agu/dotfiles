492f8c2be55da6d17a017200247076e5c4c2c6d5
Alexandre Giard <agiard@drw.com>
2020-10-23 17:26:27 -0400

    Remove rbenv Add asdf default versions

--- asdf/install.sh
+++ asdf/install.sh
@@ -1,8 +1,22 @@
 #!/bin/sh
 if command -v brew >/dev/null 2>&1; then
 	# brew is installed, install using brew
-	brew install asdf
+	# Also install all dependencies required by asdf or it's plugins
+	brew install coreutils curl git asdf
 else
+	# install all dependencies required by asdf or it's plugins depending on package manager.
+	if command -v apt >/dev/null 2>&1; then
+		# Also install all dependencies required by asdf or it's plugins
+		sudo apt install curl git
+	elif command -v dnf >/dev/null 2>&1; then
+		sudo dnf install curl git
+	elif command -v pacman >/dev/null 2>&1; then
+		sudo pacman -S curl git
+	elif command -v zypper >/dev/null 2>&1; then
+		sudo zypper install curl git
+	fi
+
+	# Install asdf using the default git method
 	git clone https://github.com/asdf-vm/asdf.git ~/.asdf
 	cd ~/.asdf || exit
 	git checkout "$(git describe --abbrev=0 --tags)"
@@ -19,13 +33,16 @@ if command -v asdf >/dev/null 2>&1; then
 	asdf plugin add ruby
 	asdf plugin add nodejs
 	asdf plugin add python
+	asdf plugin add dotnet-core
 
 	ruby_latest=$(asdf latest ruby)
 	nodejs_latest=$(asdf latest nodejs)
 	python_latest=$(asdf latest python)
+	dotnet_latest=$(asdf latest dotnet-core)
 
 	echo "ruby ${ruby_latest}
 nodejs ${nodejs_latest}
-python ${python_latest}" > ~/.tool-versions
+python ${python_latest}
+dotnet-core ${dotnet_latest}" > ~/.tool-versions
 
 fi
