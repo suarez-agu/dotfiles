7969bf51079ff6cd56af0af389166dcb51803699
Alexandre Giard <agiard@drw.com>
2020-10-23 15:53:36 -0400

    Add asdf remove rbenv, nvm

---
+++ asdf/install.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+if command -v brew >/dev/null 2>&1; then
+	# brew is installed, install using brew
+	brew install asdf
+else
+	git clone https://github.com/asdf-vm/asdf.git ~/.asdf
+	cd ~/.asdf || exit
+	git checkout "$(git describe --abbrev=0 --tags)"
+fi
+
+# Allow asdf to recognize version files from legacy apps (.nvmrc, .rbenvrc, etc)
+echo "legacy_version_file = yes" > ~/.asdfrc
+
+cd "$DOTFILES" || exit
+
+
+if command -v asdf >/dev/null 2>&1; then
+	# asdf is in our path. Add asdf plugins
+	asdf plugin add ruby
+	asdf plugin add nodejs
+	asdf plugin add python
+
+	# install latest language versions
+	asdf install ruby latest
+	asdf install nodejs latest
+	asdf install python latest
+fi
