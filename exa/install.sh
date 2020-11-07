e4862fc28b8c904addb2bc91502f3cf2e85a840f
Agustin Suarez <suarez.agu@gmail.com>
2020-11-07 01:15:33 -0500

    Improve exa install

--- exa/install.sh
+++ exa/install.sh
@@ -3,11 +3,10 @@ if [ "$(uname -s)" = "Darwin" ]; then
 	if command -v brew >/dev/null 2>&1; then
 		brew install exa
 	else
-		echo "Installing without brew is a bit weird at the moment, please look at the following link:"
-		echo "https://the.exa.website/install"
+		# Install Rust
+		curl https://sh.rustup.rs -sSf | sh
+		cargo install exa
 	fi
 else
-#	Could this work? Or do we just wait for the snap? https://www.tecmint.com/exa-a-replacement-for-ls-command-linux/
-	echo "Installing on linux distros is a bit weird at the moment, please look at the following link:"
-	echo "https://the.exa.website/install"
+	sudo apt install exa
 fi
