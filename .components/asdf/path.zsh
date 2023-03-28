b3f5d7f3bec841b177f15a05fc32dec3ce5643e4
Alexandre Giard <agiard@drw.com>
2023-03-28 16:31:41 -0400

    feat(direnv): setup direnv properly

--- .components/asdf/path.zsh
+++ .components/asdf/path.zsh
@@ -2,5 +2,8 @@
 # Add asdf to path
 export PATH=$PATH:$HOME/.asdf/shims:$HOME/.asdf/bin
 
-# setup direnv
-source "${XDG_CONFIG_HOME:-$HOME/.config}/asdf-direnv/zshrc"
+# setup asdf-direnv
+ASDF_DIR=$HOME/.asdf
+DIRENV_VERSION=$(asdf latest direnv)
+export ASDF_DIRENV_BIN="$ASDF_DIR/installs/direnv/$DIRENV_VERSION/bin/direnv"
+eval "$($ASDF_DIRENV_BIN hook zsh)"
