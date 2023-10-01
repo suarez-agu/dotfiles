9fb7e268121bacbb26e81fa95276e9238081c49b
Alexandre Giard <giard.alexandre@gmail.com>
2023-09-30 22:58:56 -0500

    chore(rtx): start removing asdf

--- .components/asdf/path.zsh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-# Add asdf to path
-export PATH=$PATH:$HOME/.asdf/shims:$HOME/.asdf/bin
-
-# setup asdf-direnv
-DIRENV_DIR=$(asdf where direnv)
-export ASDF_DIRENV_BIN="$DIRENV_DIR/bin/direnv"
-eval "$($ASDF_DIRENV_BIN hook zsh)"
---
+++ .components/rtx/path.zsh
@@ -0,0 +1,5 @@
+#!/bin/sh
+#export PATH=$PATH:$HOME/.asdf/shims:$HOME/.asdf/bin
+
+# setup rtx
+eval "$($HOME/bin/rtx activate zsh)"
